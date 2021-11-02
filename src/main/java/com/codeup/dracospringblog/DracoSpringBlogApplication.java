355c60124b364c2a2292f5a4c1e0ae5571376adb
Justin Reich <justinreich.dev@gmail.com>
2021-11-02 09:49:47 -0500

    initial commit

---
+++ src/main/java/com/codeup/dracospringblog/DracoSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.dracospringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DracoSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DracoSpringBlogApplication.class, args);
+    }
+
+}
